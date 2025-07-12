9b62d7a0a97b9ac5402e7d70abf18c59d471ac99
broadfield-dev <broadfield.apps@gmail.com>
2025-07-11 19:55:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,27 @@
-# In sys-explorer/setup.py
-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-# We no longer need to read requirements.txt for install_requires
-# as we will define them directly here.
-
 setup(
     name='sys-explorer',
-    version='0.1.1', # Bumped version for the change
+    version='0.1.2', # Bumped version for the fix
     author='Your Name',
     author_email='your.email@example.com',
     description='A web-based tool to explore local files and generate a combined markdown document.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/broadfield-dev/sys-explorer',
-    packages=find_packages(),
-    include_package_data=True,
     
-    # --- THIS IS THE KEY CHANGE ---
+    packages=['sysexplore'],
+    
+    include_package_data=True,
     install_requires=[
         'Flask',
         'repo-to-md @ git+https://github.com/broadfield-dev/repo_to_md.git'
     ],
-    
     entry_points={
         'console_scripts': [
             'sysexplorer=sysexplore.app:main',
